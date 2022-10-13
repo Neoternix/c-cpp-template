fee21725786f95f4810836331672c9720aa4eb9a
Neosake <70122910+Neosake@users.noreply.github.com>
2022-10-12 19:10:24 -0500

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/* Include The Required Modules. */
+#include <stdio.h>
+
+/* Declare Function Prototype's. */
+void example(/* parameters */);
+
+/* Convert Function Prototype's. */
+void example(/* parameters */) {
+    //implementation
+}
+
+/* Main Function Implementation. */
+int main(/*void or args*/) {
+    //implementation
+}
