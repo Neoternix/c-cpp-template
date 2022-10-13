8dcf2f35124e67bef6d8200fa572a5ba6925a88e
Neosake <70122910+Neosake@users.noreply.github.com>
2022-10-12 19:11:34 -0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/* Include The Required Modules. */
+#include <iostream>
+
+/* Declare Function Prototype's. */
+void example(/* parameters */);
+
+/* Convert Function Prototype's. */
+void example(/* parameters */) {
+    //implementation
+}
+
+/* Main Function Implementation. */
+int main(/*void or args*/) {
+    //implementation
+}
